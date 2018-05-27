2cf48169edd3dfd09ccaf65f06d8e9bf7f79f4ad
Xuefei Cao <xuefei_cao@brown.edu>
2018-05-27 11:58:34 -0400

    add package setting

---
+++ set_up.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+readme = open('README.md').read()
+
+setup(
+    name="CDN",
+    version="0.0.1",
+    description="Implementations of Causal Dynamic Network Analysis of fMRI",
+    author="Xuefei Cao, Xi Luo, Björn Sandstede",
+    author_email="xcstf01@gmail.com",
+    packages=['CDN'],
+    long_description=readme,
+    install_requires=[
+        "matplotlib==1.5.3",
+        "numpy==1.11.1",
+    ],
+    url='https://github.com/xuefeicao/CDN'
