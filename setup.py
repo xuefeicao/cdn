8808d3c01aebfe22e58aac0866dc7808011e2150
Xuefei Cao <xuefeicao@Xuefeis-MacBook-Pro.local>
2018-06-12 13:42:17 -0400

    new version

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup
 readme = open('README.md').read()
 
 setup(
-    name="CDN-fMRI",
+    name="cdn-fmri",
     version="0.0.0",
     description="Implementations of Causal Dynamic Network Analysis of fMRI",
     author="Xuefei Cao, Xi Luo, Bjorn Sandstede",
     author_email="xcstf01@gmail.com",
-    packages=['CDN'],
+    packages=['cdn'],
     long_description=readme,
     long_description_content_type='text/markdown',
     install_requires=[
@@ -18,6 +18,6 @@ setup(
         "six>=1.10.0",
         "scikit-learn>=0.18.1"
     ],
-    url='https://github.com/xuefeicao/CDN',
+    url='https://github.com/xuefeicao/cdn',
     include_package_data=True,
     )
